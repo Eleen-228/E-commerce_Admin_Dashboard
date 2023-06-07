b856fa94e159204861f293ae66ea32132ae91100
Eleen-228 <109869625+Eleen-228@users.noreply.github.com>
2023-06-07 14:45:35 -0400

    update fire config

--- firebase.config.js
+++ firebase.config.js
@@ -4,12 +4,12 @@ import { getAuth } from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: process.env.FIREBASE_API_KEY,
-	authDomain: process.env.AUTH_DOMAIN,
-	projectId: process.env.PROJECTID,
-	storageBucket: process.env.STORAGEBUCKET,
-	messagingSenderId: process.env.MESSAGINGSENDERID,
-	appId: process.env.APPID,
+	apiKey: `${process.env.FIREBASE_API_KEY}`,
+	authDomain: `${process.env.AUTH_DOMAIN}`,
+	projectId: `${process.env.PROJECTID}`,
+	storageBucket: `${process.env.STORAGEBUCKET}`,
+	messagingSenderId: `${process.env.MESSAGINGSENDERID}`,
+	appId: `${process.env.APPID}`,
 }
 
 // Initialize Firebase

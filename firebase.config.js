a6db1698cf09725bd4037f3e9310614b995f6795
Eleen-228 <109869625+Eleen-228@users.noreply.github.com>
2023-06-07 17:29:48 -0400

    update .env

--- firebase.config.js
+++ firebase.config.js
@@ -1,18 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
-require('dotenv').config()
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: `${process.env.FIREBASE_API_KEY}`,
-	authDomain: `${process.env.FIREBASE_AUTH_DOMAIN}`,
-	projectId: `${process.env.FIREBASE_PROJECT_ID}`,
-	storageBucket: `${process.env.FIREBASE_STORAGEBUCKET}`,
-	messagingSenderId: `${process.env.FIREBASE_MESSAGINGSENDERID}`,
-	appId: `${process.env.FIREBASE_APP_ID}`,
+	apiKey: `${process.env.NEXT_PUBLIC_FIREBASE_API_KEY}`,
+	authDomain: `${process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN}`,
+	projectId: `${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}`,
+	storageBucket: `${process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET}`,
+	messagingSenderId: `${process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERID}`,
+	appId: `${process.env.NEXT_PUBLIC_FIREBASE_APP_ID}`,
 }
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)

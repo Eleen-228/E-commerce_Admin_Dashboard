c5a9cac76d1365ed4a520649e804e73b204cd0c8
Eleen-228 <109869625+Eleen-228@users.noreply.github.com>
2023-06-07 20:29:41 -0400

    remove NEXT_PUBLIC prefix

--- firebase.config.js
+++ firebase.config.js
@@ -4,14 +4,14 @@ import { getAuth } from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-	apiKey: `${process.env.NEXT_PUBLIC_FIREBASE_API_KEY}`,
-	authDomain: `${process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN}`,
-	projectId: `${process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID}`,
-	storageBucket: `${process.env.NEXT_PUBLIC_FIREBASE_STORAGEBUCKET}`,
-	messagingSenderId: `${process.env.NEXT_PUBLIC_FIREBASE_MESSAGINGSENDERID}`,
-	appId: `${process.env.NEXT_PUBLIC_FIREBASE_APP_ID}`,
+	apiKey: `${process.env.FIREBASE_API_KEY}`,
+	authDomain: `${process.env.FIREBASE_AUTH_DOMAIN}`,
+	projectId: `${process.env.FIREBASE_PROJECT_ID}`,
+	storageBucket: `${process.env.FIREBASE_STORAGEBUCKET}`,
+	messagingSenderId: `${process.env.IREBASE_MESSAGINGSENDERID}`,
+	appId: `${process.env.FIREBASE_APP_ID}`,
 }
-console.log(firebaseConfig)
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
 export const auth = getAuth(app)
